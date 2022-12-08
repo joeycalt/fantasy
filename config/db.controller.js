6f32c278d2dbb42801deaae58c21d8a0a49256f9
joeycalt <joeycalt@yahoo.com>
2022-12-08 14:52:01 -0500

    fixed error

--- config/db.controller.js
+++ config/db.controller.js
@@ -5,7 +5,9 @@ require('dotenv').config();
 mongoURI = process.env.MONGODB_URI 
 // ^ HERE ^
 
-mongoose.connect(process.env.MONGODB_URI || 4000);
+mongoose.set('strictQuery', false);
+
+mongoose.connect('process.env.MONGODB_URI' || 4000);
 
 mongoose.connection.on('connected', () => {
     console.log(`[${new Date().toLocaleTimeString()}] - MongoDB connected`)
